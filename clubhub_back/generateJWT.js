d65ff2cd5fcc71d699c9b05d762deecd72712768
medazizelghazel <azizghazel5@gmail.com>
2025-04-23 12:05:10 +0100

    first commit

--- clubhub_back/generateJWT.js
+++ clubhub_back/generateJWT.js
@@ -1,14 +1,14 @@
 const jwt = require('jsonwebtoken');
 
-// Example user data (replace with actual user data if needed)
+// Example user data
 const user = {
   userId: 1,
-  role: 'Admin', // or 'Student', 'Organisator', depending on the user you want to generate the token for
+  role: 'Admin', // Or 'Student', 'Organisator'
   username: 'exampleUser'
 };
 
-// Your JWT secret key (replace with the actual secret key used in your backend)
-const secretKey = 'your-secret-key';
+// Must match JWT_SECRET in your backend .env
+const secretKey = 'secretkey';
 
 // Generate JWT with a 1-hour expiration
 const token = jwt.sign(user, secretKey, { expiresIn: '1h' });
